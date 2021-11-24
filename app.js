92bc4c91edb1fe1d98b20d7c394029ea39bb49f2
Sheena <sheena.salmon@gmail.com>
2021-11-24 00:05:07 -0500

    Updated with ramdom Cards

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
-const animals={"cat":"img/Cat.png","dog":"img/dog.png","pig":"img/pig.png"};
+const animals = { "cat": "img/Cat.png", "dog": "img/dog.png", "pig": "img/pig.png" };
+arrayCards = [];
 
-let matchPlace=document.getElementById('matchplace');
-let countClick=0;
+let matchPlace = document.getElementById('matchplace');
+let countClick = 0;
 let card1Div;
 let card2Div;
 let card1;
 let card2;
-
-  //var elem = document.getElementById('para');
-
-// let newCardContainer =document.createElement("div");
-// let newCard =document.createElement("div");
-// matchPlace.appendChild(newCardContainer);  
-// newCardContainer.appendChild(newCard);
-// newCardContainer.classList.add("card-container");
-// newCard.classList.add("card");
+let matchesFound=0;
+let playAgainButton=document.createElement("p");
+playAgainButton.innerHTML='<a href="#">Play Again</a>';
+playAgainButton.classList.add("play-again-btn");
+matchPlace.insertAdjacentElement("afterend",playAgainButton);
 
 
 
-// let image=document.createElement('img');
-// image.src=value;
-
-// newCard.appendChild(image);
-
-
-function testCards()
-{
-    if (card1==card2)
-    { 
+function testCards() {
+    if (card1 == card2) {
         console.log("true");
-        
+
         console.log(card1Div);
         console.log(card2Div);
-       return true;
+        matchesFound++;
+        console.log(matchesFound);
+        return true;
     }
-    else{
+    else {
         console.log("false");
-        
+
         console.log(card1Div);
         console.log(card2Div);
 
@@ -44,136 +35,170 @@ function testCards()
     }
 }
 
+function createAllCards() {
+
+    for (const [key, value] of Object.entries(animals)) {
+
+        let nameCardContainer = document.createElement("div");
+        // console.log(nameCardContainer);
+        let imgCardContainer = document.createElement("div");
+
+        let nameCard = document.createElement("div");
+        let imgCard = document.createElement("div");
+        let backCard = document.createElement("div");
+        let backCard2 = document.createElement("div");
+        let image = document.createElement('img');
+
+        nameCardContainer.classList.add("card-container");
+        imgCardContainer.classList.add("card-container");
+        backCard.classList.add("back-card");
+        backCard2.classList.add("back-card");
+        imgCard.classList.add("card");
+        nameCard.classList.add("card");
+        image.classList.add("card-img");
+
+       
+        imgCardContainer.appendChild(imgCard);
+        imgCardContainer.appendChild(backCard);
+
+        nameCardContainer.appendChild(nameCard);
+        nameCardContainer.appendChild(backCard2);
 
 
-for( const [key, value] of Object.entries(animals)){
+
+        image.src = value;
+
+
+        imgCard.appendChild(image);
+
+
+        imgCard.setAttribute('name', key);
+        nameCard.setAttribute('name', key);
+
+       
+
+        nameCard.innerHTML = `<span class="cardText"> ${key}</span>`;
+        nameCardContainer.addEventListener("click", function () {
+
+            cardClicked(this);
+        })
+
+
+
+        imgCardContainer.addEventListener("click", function () {
+
+            cardClicked(this);
+        })
+
+        //console.log(nameCardContainer);
+        arrayCards.push(nameCardContainer);
+        arrayCards.push(imgCardContainer)
+
+    } 
+
+
+
+
+
+}
+
+
+createAllCards();
+
+
+function randomCards()
+{
     
-let newCardContainer =document.createElement("div");
-let newCard =document.createElement("div");
-let backCard=document.createElement("div");
-backCard.classList.add("back-card");
-matchPlace.appendChild(newCardContainer);  
-newCardContainer.appendChild(newCard);
-newCardContainer.appendChild(backCard);
-newCardContainer.classList.add("card-container");
-newCard.classList.add("card");
-
-
-
-let image=document.createElement('img');
-image.src=value;
-image.classList.add("card-img");
-
-newCard.appendChild(image);
-newCard.setAttribute('name',key);
-
-newCardContainer.addEventListener("click", function(){
-    console.log(this);
+        for(let i=arrayCards.length-1; i>0;i--)
+        {
+            const j=Math.floor(Math.random()* (i+1));
+            const temp=arrayCards[i];
+            arrayCards[i]=arrayCards[j];
+            arrayCards[j]=temp;
+        }
     
-    
-    this.classList.add("no-click");
-    this.classList.toggle("flip");
-
-     console.log(this); 
-     countClick++;
-     console.log(countClick);
-    
-     if (countClick==1)
-     {
-         card1Div=this;
-         console.log(card1Div);
-         //console.log(card1);
-         card1=this.firstElementChild.getAttribute('name');
-        console.log(card1);
-     }
-     else if (countClick==2)
-      {
-          card2Div=this;
-          card2=this.firstElementChild.getAttribute("name");
-          countClick==0;
-          if (testCards())
-          {
-              countClick=0;
-             
-          }
-          else{
-            countClick=0;
-
-              card2Div.classList.toggle("flip");
-              card2Div.classList.remove("no-click");
-              card1Div.classList.toggle("flip");
-              card1Div.classList.remove("no-click");
-          }
-      }      
-
-})
- 
-
 
     
 }
 
+//randomCards();
+// console.log(arrayCards);
+
+function setCards(){
+    arrayCards.forEach(element => {
+        element.classList.remove("no-click");
+        element.classList.remove("flip");
+        matchPlace.appendChild(element);
+        
+    });
+}
+randomCards();
+setCards();
 
 
-for( const [key, value] of Object.entries(animals)){
-    
-    let newCardContainer =document.createElement("div");
-    let newCard =document.createElement("div");
-    matchPlace.appendChild(newCardContainer);  
-    newCardContainer.appendChild(newCard);
-    let backCard=document.createElement("div");
-    backCard.classList.add("back-card");
-    newCardContainer.classList.add("card-container");
-    newCardContainer.appendChild(backCard);
-    newCard.classList.add("card");
-    newCard.setAttribute('name',key);
 
-    
-    
-   newCard.innerHTML=`<span class="cardText"> ${key}</span>`;
-   newCardContainer.addEventListener("click", function(){
-    
-       this.classList.add("no-click");
-      this.classList.toggle("flip");
-    
-       console.log(this); 
-       countClick++;
-       console.log(countClick);
-      
-       if (countClick==1)
-       {
-           card1Div=this;
-           card1=this.firstElementChild.getAttribute('name');
-           console.log(card1)
-       }
-       else if (countClick==2)
-        {
-            card2Div=this;
-            card2=this.firstElementChild.getAttribute("name");
-            if (testCards())
-            {
-                countClick=0;
-             
-            }
-            else{
-                countClick=0;
-                setTimeout(()=>
-                
-               { card2Div.classList.remove("flip");
+function cardClicked(c) {
+    matchPlace.style.pointerEvents='none';
+
+    c.classList.add("no-click");
+    c.classList.toggle("flip");
+
+    console.log(c);
+    countClick++;
+    console.log(countClick);
+
+    if (countClick == 1) {
+        card1Div = c;
+        card1 = c.firstElementChild.getAttribute('name');
+        console.log(card1);
+       // matchPlace.style.pointerEvents='auto';
+    }
+    else if (countClick == 2) {
+        card2Div = c;
+        card2 = c.firstElementChild.getAttribute("name");
+        if (testCards()) {
+            countClick = 0;
+         
+
+        }
+        else {
+            countClick = 0;
+            setTimeout(() => {
+                card2Div.classList.remove("flip");
                 console.log(card2Div);
                 card2Div.classList.remove("no-click");
                 card1Div.classList.remove("flip");
                 console.log(card1Div);
-                card1Div.classList.remove("no-click");},1000
-                )
+                card1Div.classList.remove("no-click");
+            }, 800
+            )
+        }
 
-                
-            }
-        }      
-
-
-
-   })
-    
-        
+       // matchPlace.style.pointerEvents='auto';
     }
+    setTimeout(()=>{
+        matchPlace.style.pointerEvents='auto';
+    },900)
+
+}
+
+
+function startGame(){
+
+    matchesFound=0;
+    matchPlace.innerHTML="";
+    randomCards();
+  
+    setCards();
+    
+}
+
+playAgainButton.addEventListener("click", startGame);
+
+
+
+
+
+
+
+
